577f66b28af79d5a5310b4e4aa4f29dc2a0fde3d
imlonghao <github@esd.cc>
2023-12-16 11:02:19 +0000

    fix permission

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+set -e
 set -eo pipefail
 
 REPOS=$(sudo git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g' | grep -v '^\.' | grep '/' | sed 's/\/$//g')
@@ -8,5 +10,5 @@ DIRECTORY=$(pwd)
 for REPO in $REPOS
 do
     cd $DIRECTORY/$REPO/
-    makepkg --pr | tee .SRCINFO
+    makepkg --pr | sudo tee .SRCINFO
 done
