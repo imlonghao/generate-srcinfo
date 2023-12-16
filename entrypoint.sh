a17f1b5c5c414def11fdbf1ea426f8d9d252ea9b
imlonghao <github@esd.cc>
2023-12-16 10:54:42 +0000

    final version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-set -e
-set -x
 set -eo pipefail
 
-cd "$GITHUB_WORKSPACE"
-echo "workdir: $GITHUB_WORKSPACE"
-
-ls -al /workspace/imlonghao/aur
-
-REPOS=$(sudo git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g' | sed 's/\/$//g')
+REPOS=$(sudo git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g' | grep -v '^.' | sed 's/\/$//g')
 DIRECTORY=$(pwd)
 
 for REPO in $REPOS
 do
     cd $DIRECTORY/$REPO/
-    sudo makepkg --pr > .SRCINFO
+    makepkg --pr | tee .SRCINFO
 done
