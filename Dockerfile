06be86c5347b4189875f66df66810fe4a22e5147
imlonghao <github@esd.cc>
2023-12-16 06:55:22 +0000

    minify

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,11 @@
 FROM archlinux:base-devel
 
-RUN printf '[multilib]\nInclude = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
 RUN pacman -Syu --needed --noconfirm git
 
 RUN useradd -m build
 RUN echo "build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 WORKDIR /home/build
 USER build
-
-RUN git clone https://aur.archlinux.org/paru-bin.git
-RUN cd paru-bin && makepkg -si --noconfirm
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,28 +3,10 @@
 set -eo pipefail
 
 path="$1"
-archive="$2"
 
 if [[ ! -d "$path" ]]; then
     echo "::error ::Invalid path: [$path]"
     exit 1
-fi
-
-outputwarning() {
-    local warnings="$1"
-    if [[ -n "$warnings" ]]; then
-        warnings="${warnings//'%'/'%25'}"
-        warnings="${warnings//$'\n'/'%0A'}"
-        warnings="${warnings//$'\r'/'%0D'}"
-        echo "::warning::$warnings"
-    fi
-}
-
-if [[ -n "$archive" ]]; then
-    echo "::group::Use archive on $archive"
-    echo "Server = https://archive.archlinux.org/repos/$archive/\$repo/os/\$arch" | sudo tee /etc/pacman.d/mirrorlist
-    sudo pacman -Syyuu --noconfirm
-    echo "::endgroup::"
 fi
 
 abspath="$(realpath "$path")"
@@ -35,49 +17,6 @@ cd "$HOME"
 cp -r "$abspath" .
 cd "$(basename "$abspath")"
 echo "::endgroup::"
-
-echo "::group::Source PKGBUILD"
-source PKGBUILD
-echo "::endgroup::"
-
-echo "::group::Install depends"
-paru -Syu --removemake --needed --noconfirm "${depends[@]}" "${makedepends[@]}"
-echo "::endgroup::"
-
-echo "::group::Remove paru and git"
-sudo pacman -Rns --noconfirm paru-bin git || true
-echo "::endgroup::"
-
-echo "::group::List all installed packages"
-pacman -Q
-echo "::endgroup::"
-
-echo "::group::Make package"
-logfile=$(mktemp)
-makepkg -s --noconfirm 2>&1 | tee "$logfile"
-warn="$(grep WARNING "$logfile" || true)"
-outputwarning "$warn"
-echo "::endgroup::"
-
-echo "::group::Show package info"
-source /etc/makepkg.conf # get PKGEXT
-files=("${pkgname}-"*"${PKGEXT}")
-pkgfile="${files[0]}"
-echo "pkgfile=${pkgfile}" | sudo tee -a "$GITHUB_OUTPUT"
-pacman -Qip "${pkgfile}"
-pacman -Qlp "${pkgfile}"
-echo "::endgroup::"
-
-echo "::group::Install namcap"
-sudo pacman -S --needed --noconfirm namcap
-echo "::endgroup::"
-
-echo "::group::Run namcap checks"
-outputwarning "$(namcap PKGBUILD)"
-outputwarning "$(namcap "${pkgfile}")"
-echo "::endgroup::"
-
-sudo mv "$pkgfile" /github/workspace
 
 echo "::group::Generate .SRCINFO"
 makepkg --printsrcinfo > .SRCINFO
